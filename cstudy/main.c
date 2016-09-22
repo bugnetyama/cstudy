0f5d2b80ca279de78f1439c42ebe0c55210dda70
BUGNETYAMA <bugnetyama@gmail.com>
2016-09-23 00:05:42 +0900

    Initial Commit

---
+++ cstudy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  cstudy
+//
+//  Created by LEEAndon on 2016. 9. 23..
+//  Copyright © 2016년 LEEAndon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
